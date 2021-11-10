85e394f765172ef27c72dc7c4e1d615baaaa557e
vyankatesh dube <93048498+vyankatesh4@users.noreply.github.com>
2021-11-10 11:14:42 +0530

    Add files via upload

---
+++ A1 Q2'.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+word = input("Input a word to reverse: ")
+ 
+for char in range(len(word) - 1, -1, -1):
+    
+  print(word[char], end="")
+
+
+print("\n")
+
+
+# In[ ]:
+
+
+
+
